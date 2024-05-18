1d4559d3491d9898b72a8753f60b5949673f19e8
max0man0 <0naxman0@gmail.com>
2024-05-18 20:58:53 +0300

    query string

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ const port = 3000;
 
 app.get('/', (req, res, next) => {
     // TODO Get all of the medicine details from the database and replace the following hardcoded values
-    const medicines = getMedicines();
+    let medicines = getMedicines();
+
+    const expiryStatus = req.query.status;
+    if (expiryStatus) {
+        if (expiryStatus === 'expired') {
+          medicines = medicines.filter(medicine => medicine.expiryStatus === 'Expired');
+        } else if (expiryStatus === 'will-expire-soon') {
+          medicines = medicines.filter(medicine => medicine.expiryStatus === 'Will Expire Soon');
+        }
+        else if (expiryStatus === 'not-expired') {
+          medicines = medicines.filter(medicine => medicine.expiryStatus === 'Not Expired');
+        }
+    }
 
     // Send the response
     res.json(medicines);
