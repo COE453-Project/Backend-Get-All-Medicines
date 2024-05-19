e92449f2296791038adb115ef50a1571957ac184
max0man0 <0naxman0@gmail.com>
2024-05-19 14:55:42 +0300

    cors change

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: 'http://localhost:3000/'
+  origin: 'http://localhost:3000/',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['*'],
 }));
 
 app.get('/', (req, res, next) => {
