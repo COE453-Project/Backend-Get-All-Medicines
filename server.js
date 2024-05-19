af5db54060ca3223d02f6ce97f662120d4d60d01
max0man0 <0naxman0@gmail.com>
2024-05-19 15:02:14 +0300

    remove cors options

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors({
-  origin: 'http://localhost:3000/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['*'],
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000/',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['*'],
+// }));
+
+app.use(cors());
 
 app.get('/', (req, res, next) => {
     // Get all of the medicine details from the database at the link https://backend-database-olz2xjbmza-uc.a.run.app/
